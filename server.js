d6e0d712931fd377996129d0267bd537f668f22c
Tony <tonylu23@gmail.com>
2020-07-30 15:06:23 -0400

    add login POST route

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 const mockUserData = [{ name: 'Mark' }, { name: 'Jill' }]
+
+app.get('/users', (req, res) => {
+	res.json({
+		success: true,
+		message: 'successfully got users!',
+		users: mockUserData
+	})
+})
 
 app.get('/users/:id', (req, res) => {
 	console.log(req.params.id)
@@ -10,6 +21,27 @@ app.get('/users/:id', (req, res) => {
 		message: 'got one user',
 		user: req.params.id
 	})
+})
+
+app.post('/login', (req, res) => {
+	const username = req.body.username
+	const password = req.body.password
+
+	const mockUsername = 'billyTheKid'
+	const mockPassword = 'superSecret'
+
+	if (username === mockUsername && password === mockPassword) {
+		res.json({
+			success: true,
+			message: 'password and username match',
+			token: 'encrypted token goes here'
+		})
+	} else {
+		res.json({
+			success: false,
+			message: 'username and password do not match'
+		})
+	}
 })
 
 app.listen(8000, () => {
